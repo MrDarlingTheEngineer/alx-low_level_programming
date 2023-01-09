8b81a8dd99b24f6ff43f567c1aa7ce866e45d6af
MrDarlingTheEngineer <mr.darlingtheengineer@gmail.com>
2023-01-09 21:54:57 +0000

    recursion, static library

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recusion - Prints a string in reverse
+ * @s: string
+ *
+ * Retrun: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
